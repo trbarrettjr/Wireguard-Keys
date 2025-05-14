3d287f88dd95de11cd8cacc43ee4208ae12f2d77
Thomas Barrett <trbarrettjr@users.noreply.github.com>
2025-05-14 11:37:31 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, Response
 import nacl.public
 import nacl.utils
 import base64
@@ -30,6 +30,26 @@ def genkeys(count):
         psk_b64 = base64.b64encode(psk).decode('utf-8')
         keys.append(dict(privkey=pkey_b64, pubkey=pub_key_b64, psk=psk_b64))
     return jsonify(keys)
+
+@app.route("/csv/<int:count>")
+def genkeys_csv(count):
+    keys = list()
+    for i in range(count):
+        pkey = nacl.public.PrivateKey.generate()
+        pub_key = pkey.public_key
+        psk = nacl.utils.random(32)
+        pkey_b64 = pkey.encode(nacl.encoding.Base64Encoder).decode('utf-8')
+        pub_key_b64 = pub_key.encode(nacl.encoding.Base64Encoder).decode('utf-8')
+        psk_b64 = base64.b64encode(psk).decode('utf-8')
+        keys.append(dict(privkey=pkey_b64, pubkey=pub_key_b64, psk=psk_b64))
+
+    csv_out = "Private Key,Public Key,Preshared Key\n"
+    for item in keys:
+        csv_out += item['privkey'] + ',' + item['pubkey'] + ',' + item['psk'] + '\n'
+
+    response = Response(csv_out, mimetype='text/csv')
+
+    return(response)
 
 if __name__ == "__main__":
     app.run(debug = False)
